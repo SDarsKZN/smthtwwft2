069ca473ccab5670d3f827677e193bc642b385b0
sdarskzn <arslanovsamat.717@gmail.com>
2022-07-20 14:36:38 +0300

    Я сделал предыдущий код интеллектуальнее!

--- src/Task3.java
+++ src/Task3.java
@@ -1,16 +1,50 @@
 import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
-            Scanner scann = new Scanner(System.in);
-            int a = scann.nextInt();
+        //это первое нормальное\третье задание
+        Scanner scanfor1 = new Scanner(System.in);
+        int b = scanfor1.nextInt();
+        for (int str = 1; str <=b; str++){
+            System.out.println();
+            for(int i = 1; i <=str; i++){
+                System.out.print("*");
+            }
+        }
+        //это четвёртое нормальеное задание\шестое задание
+            Scanner scannfor4 = new Scanner(System.in);
+            int a = scannfor4.nextInt();
             for (int str = 1; str <= a; str++) {
                 System.out.println();
-                for (int j = str; 1 <= a-j; j++) {
+                for (int j = 1; j < str; j++) {
                     System.out.print(" ");
                 }
-                for (int num = 1; num <= str; num++) {
+                for (int num = str; num <= a; num++) {
                     System.out.print("* ");
                 }
             }
+        //это второе нормальное\четвёртое задание
+        Scanner scanfor2 = new Scanner(System.in);
+        int c = scanfor2.nextInt();
+        for (int str = 1; str <=c; str++){
+            System.out.println();
+            for (int i = str+1; i <= c; i++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <=str; j++){
+                System.out.print("*");
+            }
+        }
+        //это третье нормалльное\пятое задание
+        Scanner scanfor3 = new Scanner(System.in);
+        int d = scanfor3.nextInt();
+        for (int str = 1; str <=d; str++){
+            System.out.println();
+            for (int i = str+1; i <= d; i++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <=str; j++){
+                System.out.print("* ");
+            }
+        }
     }
 }
