19e4033ac99a9d898cab27e5507c9399ec35d7e7
sdarskzn <arslanovsamat.717@gmail.com>
2022-07-23 12:28:04 +0300

    Решето Эратосфена топ! Осталось только спомнить, как его реализовывать)))

--- src/Mass1.java
+++ src/Mass1.java
@@ -4,10 +4,10 @@ public class Mass1 {
     public static void main(String[] args) {
         //1
         int[] artrayOfInts = new int[10];
-        for(int i = 0; i < artrayOfInts.length; i++){
-            artrayOfInts[i] = i+1;
+        for (int i = 0; i < artrayOfInts.length; i++) {
+            artrayOfInts[i] = i + 1;
         }
-        for(int current : artrayOfInts){
+        for (int current : artrayOfInts) {
             System.out.println(current);
         }
         System.out.println();
@@ -15,10 +15,10 @@ public class Mass1 {
         //2
         int[] artrayOfInts2 = new int[10];
         artrayOfInts2[0] = 2;
-        for(int i = 1; i < artrayOfInts2.length; i++){
-            artrayOfInts2[i] = artrayOfInts2[i-1] + 2;
+        for (int i = 1; i < artrayOfInts2.length; i++) {
+            artrayOfInts2[i] = artrayOfInts2[i - 1] + 2;
         }
-        for(int current : artrayOfInts2){
+        for (int current : artrayOfInts2) {
             System.out.println(current);
         }
         System.out.println();
@@ -26,25 +26,24 @@ public class Mass1 {
         //3
         int[] artrayOfInts3 = new int[10];
         artrayOfInts3[0] = 1;
-        for(int i = 1; i < artrayOfInts3.length; i++){
-            artrayOfInts3[i] = artrayOfInts3[i-1] + 2;
+        for (int i = 1; i < artrayOfInts3.length; i++) {
+            artrayOfInts3[i] = artrayOfInts3[i - 1] + 2;
         }
-        for(int current : artrayOfInts3){
+        for (int current : artrayOfInts3) {
             System.out.println(current);
         }
         System.out.println();
         System.out.println();
         //4
         int[] artrayOfInts4 = new int[10];
-        for(int i = 0; i < artrayOfInts4.length; i++){
-            if(i % 2 == 0) {
+        for (int i = 0; i < artrayOfInts4.length; i++) {
+            if (i % 2 == 0) {
                 artrayOfInts4[i] = 1;
-            }
-            else {
+            } else {
                 artrayOfInts4[i] = -1;
             }
         }
-        for(int current : artrayOfInts4){
+        for (int current : artrayOfInts4) {
             System.out.println(current);
         }
         System.out.println();
@@ -52,15 +51,14 @@ public class Mass1 {
         //5
         int[] artrayOfInts5 = new int[10];
         artrayOfInts5[0] = 1;
-        for(int i = 1; i < artrayOfInts5.length; i++){
-            if(i % 2 == 0) {
-                artrayOfInts5[i] = -artrayOfInts5[i-1]+1;
-            }
-            else {
-                artrayOfInts5[i] = -artrayOfInts5[i-1]-1;
+        for (int i = 1; i < artrayOfInts5.length; i++) {
+            if (i % 2 == 0) {
+                artrayOfInts5[i] = -artrayOfInts5[i - 1] + 1;
+            } else {
+                artrayOfInts5[i] = -artrayOfInts5[i - 1] - 1;
             }
         }
-        for(int current : artrayOfInts5){
+        for (int current : artrayOfInts5) {
             System.out.println(current);
         }
         System.out.println();
@@ -69,10 +67,10 @@ public class Mass1 {
         int[] artrayOfInts6 = new int[10];
         artrayOfInts6[0] = 1;
         artrayOfInts6[1] = 1;
-        for(int i = 2; i < artrayOfInts6.length; i++){
-            artrayOfInts6[i] = artrayOfInts6[i-1] + artrayOfInts6[i-2];
+        for (int i = 2; i < artrayOfInts6.length; i++) {
+            artrayOfInts6[i] = artrayOfInts6[i - 1] + artrayOfInts6[i - 2];
         }
-        for(int current : artrayOfInts6){
+        for (int current : artrayOfInts6) {
             System.out.println(current);
         }
         System.out.println();
@@ -80,10 +78,10 @@ public class Mass1 {
         //7
         int[] artrayOfInts7 = new int[10];
         artrayOfInts7[0] = 1;
-        for(int i = 1; i < artrayOfInts7.length; i++){
-            artrayOfInts7[i] = artrayOfInts7[i-1] * (i+1);
+        for (int i = 1; i < artrayOfInts7.length; i++) {
+            artrayOfInts7[i] = artrayOfInts7[i - 1] * (i + 1);
         }
-        for(int current : artrayOfInts7){
+        for (int current : artrayOfInts7) {
             System.out.println(current);
         }
         System.out.println();
@@ -91,15 +89,14 @@ public class Mass1 {
         //last
         int[] artrayOfIntsl = new int[10];
         artrayOfInts5[0] = 1;
-        for(int i = 1; i < artrayOfIntsl.length; i++){
-            if(i % 2 == 0) {
-                artrayOfIntsl[i] = -artrayOfIntsl[i-1]+1;
-            }
-            else {
-                artrayOfIntsl[i] = -artrayOfIntsl[i-1]-1;
+        for (int i = 1; i < artrayOfIntsl.length; i++) {
+            if (i % 2 == 0) {
+                artrayOfIntsl[i] = -artrayOfIntsl[i - 1] + 1;
+            } else {
+                artrayOfIntsl[i] = -artrayOfIntsl[i - 1] - 1;
             }
         }
-        for(int current : artrayOfIntsl){
+        for (int current : artrayOfIntsl) {
             System.out.println(current);
         }
         //Второе большое задание, весёлое, позитивное
@@ -116,20 +113,20 @@ public class Mass1 {
         int maks = mass1[0];//здесь ищем максмальный элемент
         int maksel = 0;
         for (int i = 0; i < elem; i++) {
-            if(mass1[i] > maks){
+            if (mass1[i] > maks) {
                 maks = mass1[i];
-                maksel = i ;
+                maksel = i;
             }
         }
         System.out.println(maks);//вывели максимальный элемент
 
 
         int min = mass1[0];//здесь ищем минимальный элемент
-        int minel = 0 ;
+        int minel = 0;
         for (int i = 0; i < elem; i++) {
-            if(mass1[i] < min){
+            if (mass1[i] < min) {
                 min = mass1[i];
-                minel = i ;
+                minel = i;
             }
         }
         System.out.println(min);//вывели минималный элемент
@@ -139,13 +136,37 @@ public class Mass1 {
         for (int i = 0; i < mass1.length; i++) {
             sr = sr + mass1[i];
         }
-        sr = sr/ mass1.length;
-        System.out.println(sr);//вывели среднее арифметическое
+        sr = sr / mass1.length;
+        System.out.println("sr: " + sr);//вывели среднее арифметическое
 
         //теперь количество элементов между минимальным и максимальпным элементом
-        System.out.println(Math.abs(maksel - minel) - 1);
+        if (mass1.length > 1) {
+            System.out.print("Kolvoitd: ");
+            System.out.println(Math.abs(maksel - minel) - 1);
+        }
+
 
         //первое простое число
+        int b = 0;
+        for (int i = 0; i < mass1.length; i++) {
+            System.out.println("я сюда зашел");
+            if (mass1[i] == 2) {
+                System.out.println("и сюда");
+                System.out.println("2");
+                break;//до этого момента всё правильно
+            }
+            else if (mass1[i] > 2){
+                for (int j = 0; j*j < mass1[i]; j++) {
+                    System.out.println("Da.");
+                }
 
+
+
+
+
+
+
+            }
+        }
     }
 }
